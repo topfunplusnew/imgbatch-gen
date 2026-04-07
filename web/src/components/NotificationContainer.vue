92dcf5be4f303514bdb91455f811cc4abc53d39f
dongao04 <dongao04@meituan.com>
2026-04-07 18:18:22 +0800

    fix: 通知提示被弹窗遮挡，z-index从50提升到99999

--- web/src/components/NotificationContainer.vue
+++ web/src/components/NotificationContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed top-4 right-4 z-50 flex flex-col gap-2 pointer-events-none">
+  <div class="fixed top-4 right-4 flex flex-col gap-2 pointer-events-none" style="z-index: 99999;">
     <TransitionGroup name="notification">
       <div
         v-for="notif in notifications"
